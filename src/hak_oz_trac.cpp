54f1b0a8616d307e3dd9b12737072aaae43362a3
gmescudero <gmorenoescudero@gmail.com>
2022-11-11 21:16:48 +0100

    Improved ros node for gn2 calibration

--- src/hak_oz_trac.cpp
+++ src/hak_oz_trac.cpp
@@ -38,8 +38,6 @@ class Hak {
 private:
   ERROR_CODE status; // The error code of the HAK library
   sensor_msgs::JointState jointsMsg; // Standard joints positions to be published
-  sensor_msgs::Imu imuMsg1; // Standard IMU data retrieved from OpenZen first imu
-  sensor_msgs::Imu imuMsg2; // Standard IMU data retrieved from OpenZen second imu
   int imusNum; // Number of IMUs connected
   double timestamp; // Timestamp in ms
   double elbowRotFE[3] = {0,0,1}; // Rotation vector 1 of the elbow from the sensor coordinates
@@ -60,6 +58,54 @@ private:
   }
 
   /**
+   * @brief Go over the calibration process
+   * 
+   */
+  void _hakCalibration() {
+    /* Update the observations buffer */
+    if (RET_OK == status) status = cal_gn2_observations_from_database_update();
+
+    switch (calibrationStep) {
+      case NOT_DONE: 
+        /* Start data gathering */
+        ROS_INFO("\n\n\t\tPerform arbitrary motions of the elbow including flexion/extension and pronation/supination\n");
+        calibrationStep = GATHERING_DATA;
+        calibrationStartTime = timestamp;
+        break;
+      case GATHERING_DATA:
+        if (CALIB_TWO_ROT_AXES_WINDOW <= db_field_buffer_current_size_get(DB_CALIB_OMEGA,0)) {
+          /* Perform calibration algorithm */
+          ROS_INFO("\n\n\t\tData gathered\n");
+          calibrationStep = ZEROING;
+          calibrationStartTime = timestamp;
+          ROS_INFO("\n\n\t\tStand in T-Pose to calibrate\n");
+          if (RET_OK == status) status = cal_gn2_two_rot_axes_calib(elbowRotFE,elbowRotPS);
+        }
+        else if (2000 < timestamp - calibrationStartTime) {
+          /* Log current progress in data gathering */
+          ROS_INFO("Current observations count: %d/%d",
+            db_field_buffer_current_size_get(DB_CALIB_OMEGA,0),CALIB_TWO_ROT_AXES_WINDOW);
+          calibrationStartTime = timestamp;
+        }
+        break;
+      case ZEROING:
+        /* Set the zero position */
+        calibrationStep = DONE;
+        if (RET_OK == status) status = arm_elbow_angles_zero(
+          0.0,0.0,q_sensors[0],q_sensors[1],elbowRotFE,elbowRotPS);
+        if (RET_OK == status) ROS_INFO("\n\n\t\tZero position set\n");
+        else                  ROS_ERROR("Failed to perform zero procedure");
+        ROS_INFO("\tRotation vector 1:[%f,%f,%f]", elbowRotFE[0],elbowRotFE[1], elbowRotFE[2]);
+        ROS_INFO("\tRotation vector 2:[%f,%f,%f]", elbowRotPS[0],elbowRotPS[1], elbowRotPS[2]);
+        break;
+      case DONE:
+      default:
+        /* TODO: autocalib */
+        break;
+    }
+  }
+
+  /**
    * @brief Retrieve joint angles from database and fill joints message
    * 
    */
@@ -75,9 +121,9 @@ private:
     /* Compute current elbow angles */
     if (RET_OK == status) {
       double z_vect[] = { 0,0,1};
-      double x_vect[] = {-1,0,0};
+      double x_vect[] = {1,0,0};
       status = arm_elbow_angles_from_rotation_vectors_get(
-      q_sensors[0],q_sensors[1],z_vect,x_vect,elbow);
+      q_sensors[0],q_sensors[1],elbowRotFE,elbowRotPS,elbow);
       if (RET_OK != status) ROS_ERROR("Failed to compute elbow angles");
     }
     /* Fill the joint message */
@@ -91,6 +137,7 @@ private:
       jointsMsg.position.push_back( 0.0);
       jointsMsg.position.push_back( elbow[ALPHA_FE]);
       jointsMsg.position.push_back( elbow[GAMMA_PS]);
+      // ROS_INFO("Carrying: %f",elbow[BETA_CARRYING]);
     }
   }
 
@@ -107,7 +154,7 @@ private:
     double linAcc[3] = {msg->linear_acceleration.x,msg->linear_acceleration.y,msg->linear_acceleration.z};
     double angVel[3] = {msg->angular_velocity.x,msg->angular_velocity.y,msg->angular_velocity.z};
     /* TODO: Sometimes, IMUs rotating axes are swapped for unknown reasons and mess with everything else that uses them. 
-            Find a way to detect/avoid that swapping. */
+            Find a way to detect/avoid that swapping. Last time checked 10/11/2022 */
     double quat[4]   = {msg->orientation.w,msg->orientation.x,msg->orientation.y,msg->orientation.z}; 
     // double quat[4]   = {msg->orientation.w,-msg->orientation.x,-msg->orientation.y,-msg->orientation.z}; 
     
@@ -182,53 +229,21 @@ public:
 
     while(ros::ok() && RET_OK == status) {
       if (true == newData) {
-        /* Update the observations buffer */
-        if (RET_OK == status) status = cal_gn2_observations_from_database_update();
+        // Go over the calibration FSM
+        _hakCalibration();
 
-        /* Calibration process */
-        if (NOT_DONE == calibrationStep) {
-          /* Start data gathering */
-          ROS_INFO("\n\n\t\tPerform arbitrary motions of the elbow including flexion/extension and pronation/supination\n");
-          calibrationStep = GATHERING_DATA;
-          calibrationStartTime = timestamp;
-        }
-        else if (GATHERING_DATA == calibrationStep && 1000 < timestamp - calibrationStartTime) {
-          ROS_INFO("Current observations count: %d/%d",
-            db_field_buffer_current_size_get(DB_CALIB_OMEGA,0),CALIB_TWO_ROT_AXES_WINDOW);
-          calibrationStartTime = timestamp;
-        }
-        else if (GATHERING_DATA == calibrationStep
-                  && CALIB_TWO_ROT_AXES_WINDOW <= db_field_buffer_current_size_get(DB_CALIB_OMEGA,0)) 
-        {
-          /* Perform calibration algorithm */
-          ROS_INFO("\n\n\t\tData gathered\n");
-          calibrationStep = ZEROING;
-          calibrationStartTime = timestamp;
-          ROS_INFO("\n\n\t\tStand in T-Pose to calibrate\n");
-          status = cal_gn2_two_rot_axes_calib(elbowRotFE,elbowRotPS);
-        }
-        else if (ZEROING == calibrationStep && 4000 < timestamp - calibrationStartTime) {
-          /* Set the zero position */
-          calibrationStep = DONE;
-          status = arm_elbow_angles_zero(0.0,0.0,q_sensors[0],q_sensors[1],elbowRotFE,elbowRotPS);
-          if (RET_OK == status) ROS_INFO("\n\n\t\tZero position set\n");
-          else                  ROS_ERROR("Failed to perform zero procedure");
-          ROS_INFO("\tRotation vector 1:[%f,%f,%f]", elbowRotFE[0],elbowRotFE[1], elbowRotFE[2]);
-          ROS_INFO("\tRotation vector 2:[%f,%f,%f]", elbowRotPS[0],elbowRotPS[1], elbowRotPS[2]);
-        }
-
-        /* Publish the current joint angles */
-        if (RET_OK == status) {
+        if (RET_OK == status && DONE == calibrationStep) {
           // Retrieve the shoulder and elbow angles from the database and set the joints vector
           _jointsMsgFromDbSet();
-          if (RET_OK == status) pub.publish(jointsMsg);
+          pub.publish(jointsMsg);
         }
 
-        /* If iteration not executed reset error code */
+        // If iteration not executed reset error code 
         if (RET_NO_EXEC == status) status = RET_OK;
+        // If any error has been detected shut down the process
         else if (RET_OK != status) ros::shutdown();
-
-        /* Reset new data flag */
+        
+        // Reset new data flag
         if (RET_OK == status) newData = false;
       }
 
