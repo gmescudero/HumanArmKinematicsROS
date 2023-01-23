5ae4acf008d00c4cd6b63749610f807b9053478b
gmescudero <gmorenoescudero@gmail.com>
2023-01-23 20:18:13 +0100

    Improved saveRecording python script to build csv and plots

--- saveRecordig.py
+++ saveRecordig.py
@@ -1,9 +1,14 @@
-from datetime import datetime
+#!/usr/bin/python3
+
 from dataclasses import dataclass
 import pandas as pd
-from enum import IntEnum
+import matplotlib.pyplot as plt
+from cmath import pi
+import os
+
 
 FILE="elbowAnglesRecording.txt"
+FOLDER="experiment_data"
 
 """
 NEW RECORDING: limite min ps
@@ -30,28 +35,21 @@ class Recordig:
 
 rec_list = []
 
-import subprocess as sb
-#rosservice call /elbowAngles >> $FILE
-
-# result = sb.run(['rosservice','call','/elbowAngles'],capture_output=True)
-
-# print(result.stdout.split('\n'))
-
 with open(FILE,"r") as f:
     rec = None
     for line in f.readlines(): 
         if line.startswith("timestamp"):
             continue
         elif line.strip().startswith("secs"):
-            rec.t_secs = line.split(': ')[1].strip()
+            rec.t_secs = int(line.split(': ')[1].strip())
         elif line.strip().startswith("nsecs"):
-            rec.t_nsecs = line.split(': ')[1].strip()
+            rec.t_nsecs = int(line.split(': ')[1].strip())
         elif line.strip().startswith("fe"):
-            rec.fe = line.split(': ')[1].strip()
+            rec.fe = float(line.split(': ')[1].strip())*180/pi
         elif line.strip().startswith("ps"):
-            rec.ps = line.split(': ')[1].strip()
+            rec.ps = float(line.split(': ')[1].strip())*180/pi
         elif line.strip().startswith("beta"):
-            rec.beta = line.split(': ')[1].strip()
+            rec.beta = float(line.split(': ')[1].strip())*180/pi
         elif line.startswith("NEW"):
             if rec is None:
                 rec = Recordig(recType=rec_types[0])
@@ -63,6 +61,43 @@ with open(FILE,"r") as f:
     rec_list.append(rec)
 
 # print(rec_list)
-print(pd.DataFrame(rec_list))
+t_init = rec_list[0].t_secs
+
+file = f"{FOLDER}/data_{t_init}"
+if not os.path.exists(file):
+    os.makedirs(file)
+
+
 df = pd.DataFrame(rec_list)
-# df.to_csv("pollo.csv")
+print(df)
+df.to_csv(f"{file}/data_{t_init}.csv")
+
+fe_max = {}
+fe_min = {}
+ps_max = {}
+ps_min = {}
+
+for rec in rec_list:
+    if      rec.recType == 'FE_MAX': fe_max.update({rec.t_secs-t_init:rec.fe})
+    elif    rec.recType == 'FE_MIN': fe_min.update({rec.t_secs-t_init:rec.fe})
+    elif    rec.recType == 'PS_MAX': ps_max.update({rec.t_secs-t_init:rec.ps})
+    elif    rec.recType == 'PS_MIN': ps_min.update({rec.t_secs-t_init:rec.ps})
+    else: print("Unknown rec type")
+
+plt.plot(fe_max.keys(),fe_max.values(), fe_min.keys(), fe_min.values())
+plt.grid(True)
+plt.title("Flexion-Extension limits over time")
+plt.xlabel("time (s)")
+plt.ylabel("angle (º)")
+plt.legend(("fe_min","fe_max"))
+plt.savefig(f"{file}/fe_{t_init}.png")
+plt.close()
+
+plt.plot(ps_max.keys(),ps_max.values(), ps_min.keys(), ps_min.values())
+plt.grid(True)
+plt.title("Pronation-Supination limits over time")
+plt.xlabel("time (s)")
+plt.ylabel("angle (rad)")
+plt.legend(("ps_min","ps_max"))
+plt.savefig(f"{file}/ps_{t_init}.png")
+plt.close()
